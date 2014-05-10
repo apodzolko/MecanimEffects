28a5632204c15a9072e179753963c6414899b445
apodzolko <apodzolko@gmail.com>
2014-05-10 22:00:43 +0400

    Reset support for particle systems.

---
+++ ParticleSystemReset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MecanimEffects {
+	/// <summary>
+	/// Hack to reset particle systems.
+	/// </summary>
+	public class ParticleSystemReset : MonoBehaviour {
+		/// <summary>
+		/// The particle system to reset.
+		/// </summary>
+		public ParticleSystem particleSystem;
+		/// <summary>
+		/// Resets the particle system.
+		/// </summary>
+		private void Reset() {
+			if(particleSystem == null) {
+				particleSystem = GetComponent<ParticleSystem>();
+			}
+			if(particleSystem == null) return;
+			particleSystem.Stop(true);
+			particleSystem.Clear(true);
+			particleSystem.Play(true);
+		}
+	}
+}
